410ab81529e14d34629c5ca3511d526495964ec8
zvryn <sarinkanok@gmail.com>
2024-10-24 09:57:33 +0700

    add appbar

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post/Post";
+
+const Posts = () => {
+  return (
+    <div>
+      <div>POSTS</div>
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
