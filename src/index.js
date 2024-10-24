60e1b2a734f2019ced16b49ad65f2ec9aff5b0bc
zvryn <sarinkanok@gmail.com>
2024-10-24 10:57:41 +0700

    init redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
+import reducers from "./reducers";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
